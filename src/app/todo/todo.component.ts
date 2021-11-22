39b160f78c266aedcc5b84b386845c6af45b095f
AurelBouchet <aurelien-bouchet@hotmail.fr>
2021-11-22 21:01:37 +0100

    localstorage wip

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,18 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import Swal from 'sweetalert2';
-
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.scss'],
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit {
   constructor() {
-    this.todo = [];
+    this.todos = [];
   }
-
-  todo: any;
-  inputTask: any;
+  ngOnInit() {
+    this.lastTask = localStorage.getItem('2');
+    console.log(this.lastTask);
+    this.todos.push(this.lastTask);
+  }
+  lastTask: any | [];
+  id: number = Date.now();
+  task: any;
+  todos: any;
   alert = Swal.mixin({
     toast: true,
     position: 'top-start',
@@ -22,18 +27,19 @@ export class TodoComponent {
   });
 
   addTask() {
-    this.inputTask
-      ? this.todo.push(this.inputTask)
+    this.task
+      ? this.todos.push(this.task)
       : this.alert.fire({
           icon: 'warning',
           iconColor: '#f27474',
           title: 'Aucune tâche à ajouter',
         });
-    this.inputTask = '';
+    localStorage.setItem(JSON.stringify(this.id), JSON.stringify(this.task));
+    this.task = '';
   }
 
   deleteTask(i: number) {
-    this.todo.splice(i, 1);
+    this.todos.splice(i, 1);
     this.alert.fire({
       icon: 'success',
       title: 'Votre tâche a bien été supprimée!',
