cd7f8118a769a375dae41f6b07a2ab9fcd53b543
AurelBouchet <aurelien-bouchet@hotmail.fr>
2021-12-06 21:49:51 +0100

    refactor pictures

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
-import { ChildComponent } from './child/child.component';
 
 @NgModule({
-  declarations: [AppComponent, TodoComponent, ChildComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+  declarations: [TodoComponent, AppComponent],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,80 +1,13 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import Swal from 'sweetalert2';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-todo',
+  selector: 'app-child',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.scss'],
 })
-export class TodoComponent implements OnInit {
-  constructor() {
-    this.todos = [];
-  }
-
-  recordedList: any;
-  todos: any;
-  value: any;
-  ngOnInit() {
-    this.recordedList = JSON.parse(localStorage.getItem('list') || '[]');
-    if (this.todos) {
-      for (let todo of this.recordedList) {
-        this.todos.push(todo);
-      }
-    } else {
-      this.todos;
-    }
-  }
-
-  alert = Swal.mixin({
-    toast: true,
-    position: 'top',
-    showConfirmButton: false,
-    timerProgressBar: true,
-    timer: 2000,
-  });
-
-  addTodo(todo: string) {
-    this.todos.push(todo);
-    localStorage.setItem('list', JSON.stringify(this.todos));
-  }
-
-  deleteTodo(i: number) {
-    this.todos.splice(i, 1);
-    const list = JSON.parse(localStorage.getItem('list') || '{}');
-    for (let i of list) localStorage.removeItem(this.todos);
-    this.alert.fire({
-      icon: 'success',
-      title: 'Votre tâche a bien été supprimée!',
-    });
-    localStorage.setItem('list', JSON.stringify(this.todos));
-  }
-
-  swal = Swal.mixin({
-    customClass: {
-      confirmButton: 'btn btn-success',
-      cancelButton: 'btn btn-danger',
-    },
-    buttonsStyling: false,
-  });
-
-  deleteAll() {
-    this.swal
-      .fire({
-        title: 'Etes-vous sûr.e?',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonText: 'Oui, supprimer la liste',
-        cancelButtonText: 'Annuler',
-        reverseButtons: true,
-      })
-      .then((result) => {
-        if (result.isConfirmed) {
-          this.swal.fire('Votre liste a été supprimée!', '', 'success');
-          localStorage.removeItem('list');
-          this.todos = [];
-        } else {
-          null;
-        }
-      });
+export class TodoComponent {
+  @Output() newTodo = new EventEmitter<string>();
+  addTodo(value: string) {
+    value ? this.newTodo.emit(value) : null;
   }
 }
