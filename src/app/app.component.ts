794c4ed59b0301ca50108121dd24d91a4fec2065
AurelBouchet <aurelien-bouchet@hotmail.fr>
2021-12-17 20:37:28 +0100

    tailwind initialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss', './../tailwind.scss'],
 })
 export class AppComponent implements OnInit {
   constructor() {
