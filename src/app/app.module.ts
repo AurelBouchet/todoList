be5c7aeceab7406b26b1adc658d816386166813e
AurelBouchet <aurelien-bouchet@hotmail.fr>
2023-09-07 13:48:40 +0200

    task store and user components deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { User2Component } from './user2/user2.component';
-import { User1Component } from './user1/user1.component';
-import { TaskRepository } from './store/task.store';
 
 @NgModule({
-  declarations: [AppComponent, User2Component, User1Component],
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule, AppRoutingModule],
-  providers: [TaskRepository],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
